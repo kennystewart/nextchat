d9c26b7d13b1aaf645c98e040478bd7f061018ce
Chris Trenka <chris@trenkas.com>
2022-11-30 16:54:31 -0500

    removed signin.js as txt

--- components/Collapse.tsx
+++ components/Collapse.tsx
@@ -2,12 +2,13 @@
 import React, { useState } from "react";
 import { FaAngleDown } from "react-icons/fa";
 
-const Collapse = () => {
+const Collapse = (props) => {
+  console.log(props.faq);
   const [open, setOpen] = useState(false);
   return (
     <div className="flex flex-col">
       <div className="flex p-4 font-medium text-lg items-center justify-between md:text-2xl">
-        Is bitcoin gambling legal?
+        {props.faq.question}
         <FaAngleDown
           className="mx-4 text-lg font-thin"
           onClick={() => setOpen(!open)}
@@ -18,13 +19,7 @@ const Collapse = () => {
           open ? "flex" : "hidden"
         }`}
       >
-        Online casinos have everything it takes to become more popular than land
-        based ones, especially among those who are only starting their journey
-        in the gambling world. That said, it may be too difficult to choose
-        between hundreds of platforms to play with. If it is your case, it s
-        time to leave all those worries to Allfreechips! We will provide you
-        with the online casino guide and help you turn their promotional offers
-        to your advantage.
+        {props.faq.answer}
       </div>
     </div>
   );
--- components/faq.tsx
+++ components/faq.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Collapse from "./Collapse";
 const Faq = (props) => {
+    const question = "Why Are we not done yest?";
+    const answer = "Bacause were going very slow";
+    const faq= {question,answer};
 return (
 <div id="faq" className="">
   <h3 className="text-3xl font-semibold my-6 md:text-4xl md:my-10">
     Frequently asked questions
   </h3>
   <hr className="border-sky-700 dark:border-white" />
-  <Collapse />
+  <Collapse faq={faq}/>
   <hr className="border-sky-700 dark:border-white" />
-  <Collapse />
-  <hr className="border-sky-700 dark:border-white" />
-  <Collapse />
-  <hr className="border-sky-700 dark:border-white" />
-  <Collapse />
+  <Collapse faq={faq}/>
 </div>
     );
 };
