5d9b5e3cdd2cf4074f74072911cc26030d1fc55b
ctrenks <chris@trenkas.com>
2023-01-14 14:59:18 -0500

    rename app folder

---
+++ app/head.tsx
@@ -0,0 +1,17 @@
+export default function Head() {
+  return (
+    <>
+      <title>AllFreeChips</title>
+      <meta
+        name="keywords"
+        content="Online Casino Guide, 2022 online casinos, Online Casinos, casino bonus codes, Casino Guide, Casino Reviews"
+      />
+      <meta
+        name="description"
+        content="AllFreeChips is the biggest community with over 30,038+ active members bringing the best online casino bonuses. Latest casino bonus codes of 2022"
+      />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
