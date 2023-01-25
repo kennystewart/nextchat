2be3e5cdfab0672cbf3bd48218883fe926aa5c38
ctrenks <chris@trenkas.com>
2023-01-25 12:15:05 -0500

    added kimages to software displays

--- components/SoftBox.tsx
+++ components/SoftBox.tsx
@@ -5,6 +5,17 @@ const Casino = (props) => {
     <span className="flex flex-col items-center space-y-2 md:space-y-6 border-x-2 border-b-2 border-gray-300 shadow-md mx-2 my-4 rounded-xl md:m-6 p-6">
       
       <p className='text-xl font-medium h-7'>{props.title}</p>
+      <p>
+      <Image
+                unoptimized // avoids getting charged
+                width={175}
+                height={100}
+                alt={props.name}
+                src={`https://www.allfreechips.com/image/software/${encodeURIComponent(
+                  props.image
+                )}`}
+              />
+      </p>
       <p className='text-xl font-medium'>Casinos : {props.count}</p>
       <p className='text-xl font-medium'>Slots : {props.games}</p>
     </span>
