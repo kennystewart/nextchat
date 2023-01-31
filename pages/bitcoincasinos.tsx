0d2ddf5c912d3c0a535be0afc70a910ac8d5311d
ctrenks <chris@trenkas.com>
2023-01-31 18:24:41 -0500

    updating Bitcoincasino page

--- pages/bitcoincasinos.tsx
+++ pages/bitcoincasinos.tsx
@@ -58,26 +58,58 @@ const PageOut = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
   const authorData = { author, authorText };
   const [show, setShow] = useState(true);
   const data = props.data;
-  // const likeCasinoData = props.bdata;
-  // const gameList = props.gamedata;
-  // const casinoname = likeCasinoData[0].casino;
-  // const casinoid = likeCasinoData[0].id;
-  // const casinoData = { casinoid, casinoname };
-  // const gameListData = { gameList, casinoData };
-  // const gameReview = { __html: data.review[0].description };
-  const title = "Title";
-  const content = "Content that relates to the title.";
-  const pros = [
-    { title: "--", content },
-    { title: title, content },
-    { title, content },
+
+  const prosCons = {
+    pros: [
+      {
+        title:
+          "USA Online Casinos",
+        content:
+          "American should be free to play any casino they want, and online casinos allowing USA players may be the best route for Americans to gamble.",
+      },
+      {
+        title: "Game whenver you like",
+        content:
+          "If you feel like playing slots at 4AM you can easily play at an online casino from your home in the US. Also if you likke to gamble in your pajamas that cool as well.",
+      },
+    ],
+    cons: [
+      {
+        title: "Banking",
+        content:
+          "The cons for USA casinos is the banking sytem, they are not allowed to transfer funds for illegal online gambling yet nobody knows what illegal online gaming is so its a strange law.",
+      },
+    ],
+  };
+
+  const faq = [
+    {
+      question: "Are Bitcoin casinos legal in th USA?",
+      answer:
+        "There is nothing illegal about Bitcoin in the US, you can freely buy it, sell it, and gamble with it. The conversion of Bitcoin to fiat currency (US dollars) is somewhat of a grey area. In the worst-case scenario, capital gains tax would apply.",
+    },
+    {
+      question: "Is Bitcoin safe to use?",
+      answer:
+        "Yes, cryptocurrencies are very safe as blockchain removes the need for a centralized entity. The only thing left to do is to pick a good online casino from the list above. Here on AllFreechips.com we're listing only reputable casinos, so you're safe if you pick one of those.",
+    },
+    {
+      question: "What are the advantages to Bitcoin Gambling?",
+      answer:
+        "The main advantage of Bitcoin gambling in USA is that deposits and withdrawals can't be blocked by a bank, which is often the case with fiat deposits as US banks block deposits to known gambling sites. This is not the case in Bitcoin, it bypasses the banks and the central authority.",
+    },
+    {
+      question: "What do I need to start gambing with Bitcoin?",
+      answer:
+        "To start gambling in Bitcoin, you first need to buy some Bitcoin via an exchange such as Coinbase. When you have BTC in your wallet, you can send it to any.",
+    },
+    {
+      question: "Can I deposit with a debit card and cashout via Bitcoin?",
+      answer:
+        "Not directly, since almost all casinos require you to deposit and withdraw using the same method. However, you may deposit US dollars via credit or debit card to a crypto exchange such as Coinbase, buy Bitcoin there, send it to a casino, and then withdraw your BTC back to Coinbase.",
+    },
   ];
-  const cons = pros;
-  const prosCons = { pros, cons };
-  const question = "Are Bitcoin casinos safe?";
-  const answer =
-    "The question really does not have a good answer.  Bitcoin is the new preferred way of financing casino transactions, this does not have any impact on whether the casino is actually safe or not.  This is why you should only play at bitcoin casinos like the ones reviewed her at Allfreechips.";
-  const faq = [{ question, answer }];
+  
   return (
     <div className="bg-white text-sky-700 dark:bg-zinc-800 dark:text-white">
       <Header />
@@ -132,14 +164,20 @@ const PageOut = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
                   </a>
                 </div>
                 <p className="font-normal pt-4 pb-2 text-justify md:text-xl md:p-6">
-                  Bitcoin Casinos are the future of online gambling without
-                  question. The speed and ease of using bitcoin is unmatched,
-                  and very secure if you use your own wallet. With all the
-                  negative news about Crypto Currency lately you should know all
-                  these losses are from exchanges where you should not keep your
-                  Bitcoin. Play safe online bitcoin casinos and store your
-                  bitcoin in a local or offline wallet where YOU control the
-                  keys.
+                  Bitcoin gambling is a fantastic way to gamble in the US as the
+                  deposits and withdrawals are easier and faster than with any
+                  other method available to US residents. Also, apart from
+                  winning in the casino you can benefit from the Bitcoin price
+                  rise and therefore grow your crypto portfolio! So choose a
+                  bitcoin casino from the comprehensive list below. All of them
+                  have some welcome offer you&apos;ll want to take advantage of,
+                  and you&apos;ll double your Bitcoin in no time if you claim
+                  one of those offers. There are casinos that give out Bitcoin
+                  no deposit bonuses and Bitcoin free spins, so you can start
+                  gambling in Bitcoin even if you don&apos;t currently have any
+                  crypto. Of course, you&apos;ll get the most bang for your buck
+                  if you take advantage one of the Bitcoin welcome bonuses as
+                  they are the most rewarding.
                 </p>
               </div>
             </div>
@@ -174,16 +212,16 @@ const PageOut = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
             </span>
 
             <span>
-              <Link href="#SlotReview"> Review</Link>
+              <Link href="#SlotReview">Bitcoin Review</Link>
             </span>
             <span>
               <Link href="#ProsCons"> Pros and Cons</Link>
             </span>
             <span>
-              <Link href="#LikeCasinos">Casinos With </Link>
+              <Link href="#LikeCasinos">Bitcoin Casinos</Link>
             </span>
             <span>
-              <Link href="#LikeSlots">Slots Like </Link>
+              <Link href="#LikeSlots">Slots using Bitcoin </Link>
             </span>
             <span>
               <Link href="#faq">FAQs</Link>
@@ -199,19 +237,16 @@ const PageOut = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
             </span>
             <div className="my-4 flex flex-col space-y-4">
               <span>
-                <Link href="#SlotReview">Review</Link>
+                <Link href="#SlotReview">Bitcoin Review</Link>
               </span>
               <span>
-                <Link href="#ProsCons">Pros and Cons</Link>
+                <Link href="#ProsCons"> Pros and Cons</Link>
               </span>
               <span>
-                <Link href="#LikeCasinos">Casinos With</Link>
+                <Link href="#LikeCasinos">Bitcoin Casinos</Link>
               </span>
               <span>
-                <Link href="#LikeSlots">Slots Like</Link>
-              </span>
-              <span>
-                <Link href="#faq"> FAQs</Link>
+                <Link href="#LikeSlots">Slots using Bitcoin </Link>
               </span>
             </div>
           </div>
@@ -224,9 +259,51 @@ const PageOut = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
 
             <div>
               <h1 id="SlotReview" className="text-3xl font-semibold my-4">
-                Review
+                Playing Bitcoin USA Casinos Review
               </h1>
-              <div className="text-lg font-normal">Lots O Text HERE</div>
+              <div className="text-lg font-normal">
+                <b>Why gamble in Bitcoin?</b>{" "}
+                <p>
+                  When you&apos;re gambling in Bitcoin you never have to worry
+                  about your deposit or withdrawal being blocked. You&apos;re
+                  off the grid, the transactions are done on the decentralized
+                  blockchain, and that&apos;s the safest way to move funds. Most
+                  casinos will instantly convert your Bitcoin deposit to USD,
+                  therefore Bitcoin is only a vessel for transfering money, and
+                  you&apos;ll still be gambling in dollars. Some casinos allow
+                  gambling directly in Bitcoin.
+                </p>{" "}
+                <b>How to buy Bitcoin</b>{" "}
+                <p>
+                  The easiest way to buy Bitcoin is to head over to one of the
+                  crypto exchanges such as Coinbase, Crypto.com or Gemini. You
+                  deposit USD and buy any cryptocurrency you like, including
+                  Bitcoin. After you do that, Bitcoin will be in your wallet and
+                  you can freely send it to an online casino that accepts
+                  Bitcoin deposits, and then bring it back from the casino to
+                  your wallet when you want.{" "}
+                </p>
+                <b>Bitcoin casino payouts, deposits and fees</b>
+                <p>
+                  Bitcoin deposits and withdrawals are faster than with any
+                  other payment method. The only waiting time is when the casino
+                  is verifying the transaction. Over the past year or so,
+                  Bitcoin transaction fees were steady at around $2. Most of the
+                  time the fee per transaction is between $1.50 and $2.50 so
+                  that&apos;s what you can expect to pay whenever you send
+                  Bitcoin somewhere. This is very affordable, especially if you
+                  transfer a lot of funds. With higher amounts, Bitcoin is
+                  absolutely the cheapest way to move money, as the transaction
+                  fee is fixed and doesn'&apos;t depend on the amount you send.
+                  Some casinos even pay the fee on your behalf and therefore let
+                  you withdraw your Bitcoin absolutely free, though the usual
+                  $1.50-2.50 fee per transaction isn&apos;t much either. Pay
+                  attention to the exchange rate and any applicable fees when
+                  you&apos;re buying and selling Bitcoin at an exchange, though.
+                  Fees will also apply when you&apos;ve converted your Bitcoin
+                  to USD and are withdrawing USD to your bank account.
+                </p>
+              </div>
               <ProsCons data={prosCons} />
               <div className="text-lg font-normal">
                 <h3 className="text-3xl font-semibold my-6 md:text-4xl md:my-10">
