3c561bd9397b763e42e26eaf37049503d46ca984
ctrenks <chris@trenkas.com>
2023-02-01 14:54:26 -0500

    Added en as default lang in next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "fr", // default lang fr
+  },
   reactStrictMode: false,
   images: {
     domains: [
