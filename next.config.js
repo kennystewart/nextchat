7d1dff884d822b547a7471bcb45418da659ef961
ctrenks <chris@trenkas.com>
2023-03-26 17:21:25 -0400

    fix i18n

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en", // default lang fr
-  },
   reactStrictMode: false,
   images: {
     domains: [
