3b795a6ec2435228637dd5d0e9a846369b3a9123
ctrenks <chris@trenkas.com>
2023-04-23 21:23:47 -0400

    get rid of effects

--- app/no-deposit-casinos/NoDepositCasinoList.tsx
+++ app/no-deposit-casinos/NoDepositCasinoList.tsx
@@ -1,43 +1,29 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { FaAngleDown } from "react-icons/fa";
 import CasinoDisplayList from "../../components/CasinoDisplayList";
 
 const NoDepositCasinoList = (props: NoDepositCasinoList) => {
-  const [pageNumber, setPageNumber] = useState<number>(1);
-
+  const pageNumber = useRef<number>(1);
   const [casinoData, setCasinoData] = useState<string[]>(props.bonus);
-  useEffect(() => {
-    pageNumber > 1 &&
-      fetch(`/api/noDeposit/?pageNumber=${pageNumber}`)
-        .then((res) => {
-          if (!res.ok) {
-            throw res;
-          }
-          return res.json();
-        })
-        .then((res) => setCasinoData([...casinoData, ...res.bonus]))
-        .catch((err) => {});
-  }, [pageNumber]);
-
-  useEffect(() => {
-    fetch(`/api/noDeposit/?pageNumber=${pageNumber}`)
-      .then((res) => {
-        if (!res.ok) {
-          throw res;
-        }
-        return res.json();
-      })
-      .then((res) => setCasinoData(res.bonus))
-      .catch((err) => {});
-  }, []);
   return (
     <div>
       <CasinoDisplayList data={casinoData} />
       <div
         className="flex justify-center items-center text-xl font-medium md:text-3xl py-2 md:py-6 cursor-pointer"
         onClick={() => {
-          setPageNumber(pageNumber + 1);
+          fetch(`/api/noDeposit/?pageNumber=${pageNumber.current + 1}`)
+            .then((res) => {
+              if (!res.ok) {
+                throw res;
+              }
+              return res.json();
+            })
+            .then((res) => {
+              setCasinoData([...casinoData, ...res.bonus]);
+              pageNumber.current = pageNumber.current + 1;
+            })
+            .catch((err) => {});
         }}
       >
         Show more
